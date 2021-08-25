7c9e81147beb63ee1e3984c294db2a83f7bc75ae
Shatanshu <shatanshu.1923csi1044@kiet.edu>
2021-08-25 19:33:31 +0530

    Bubble_Shooter

---
+++ Assets/Scripts/SponseredScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SponseredScript : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] balloons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartSpawling());
+
+    }
+
+    // Update is called once per frame
+    IEnumerator StartSpawling()
+    {
+        yield return new WaitForSeconds(4);
+        for(int i = 0; i < 3; i++)
+        {
+            Instantiate(balloons[i], spawnPoints[i].position, Quaternion.identity);
+        }
+        StartCoroutine(StartSpawling());
+    }
+}
